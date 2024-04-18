6060162a1058f67d8fd7743584a298dc52ffe4bb
Tre' Smith <admitthisone@gmail.com>
2024-04-18 14:02:25 -0500

    home content types

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { attributes, react as HomeContent } from '../content/home.md'
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes
+    let { title } = attributes
     return (
       <>
         <Head>
@@ -13,14 +13,6 @@ export default class Home extends Component {
         <article>
           <h1>{title}</h1>
           <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
         </article>
       </>
     )
