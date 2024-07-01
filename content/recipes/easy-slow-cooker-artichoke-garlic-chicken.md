fe77d2da0a11275fd407b8edd7037f7dce080c7c
Jerry Sirisavath <Jsirisavath123@gmail.com>
2024-07-01 17:14:09 -0400

    Initial work to set up recipe-mock-up.js and adding recipes.

--- content/recipes/easy-slow-cooker-artichoke-garlic-chicken.md
+++ content/recipes/easy-slow-cooker-artichoke-garlic-chicken.md
@@ -19,8 +19,8 @@ summary: '`"If you have around <b>45 minutes</b> to spend in the kitchen, Easy
   & Artichoke Slow Cooker Chicken</a>."`'
 dairyFree: true
 servings: 2
-prepTime: "45"
-cookTime: "45"
+prepTime: '45'
+cookTime: '45'
 featuredImage: https://img.spoonacular.com/recipes/642093-556x370.jpg
 ingredients:
   - ingredient: 1 cup Quinoa
@@ -38,15 +38,9 @@ ingredients:
   - ingredient: 6 mint leaves
   - ingredient: 6 cloves finely minced garlic
   - ingredient: 1/2 cup lemon juice
-  - ingredient: Sea Salt
-  - ingredient: "10 grilled shrimp seasoned with lemon juice and garlic "
-instructions: '`"Chop, mince and dice all veggies & spices.\n Combine bell
-  pepper, onion, garlic, tapioca, rosemary, lemon zest, artichokes, chicken
-  thighs, salt and pepper in crockpot.\n Pour broth and lemon juice over
-  mixture.\n Cover and cook on low for 5 hours- You really dont need to check on
-  this but every once and a while maybe give it a little stir.\n Once chicken is
-  cooked through, cook pasta.\n Serve over pasta top with fresh shaved
-  parmesan\n ENJOY!!!"`'
+  - ingredient: 4 tsps Sea Salt
+  - ingredient: 10 grilled shrimp seasoned with lemon juice and garlic
+instructions: '`"If you have around <b>45 minutes</b> to spend in the kitchen, Easy Slow Cooker Artichoke Garlic Chicken might be a tremendous <b>dairy free</b> recipe to try. This main course has <b>718 calories</b>, <b>76g of protein</b>, and <b>15g of fat</b> per serving. For <b>$5.1 per serving</b>, this recipe <b>covers 42%</b> of your daily requirements of vitamins and minerals. This recipe serves 2. This recipe from Foodista requires chicken broth, lemon zest, salt & pepper, and rosemary. Not a lot of people made this recipe, and 2 would say it hit the spot. Overall, this recipe earns a <b>good spoonacular score of 79%</b>. If you like this recipe, you might also like recipes such as <a href=\"https://spoonacular.com/recipes/easy-slow-cooker-artichoke-garlic-chicken-1369311\">Easy Slow Cooker Artichoke Garlic Chicken</a>, <a href=\"https://spoonacular.com/recipes/slow-cooker-spinach-and-artichoke-chicken-1448129\">Slow Cooker Spinach and Artichoke Chicken</a>, and <a href=\"https://spoonacular.com/recipes/lemon-artichoke-slow-cooker-chicken-550756\">Lemon & Artichoke Slow Cooker Chicken</a>."`'
 videoTitle: fdas
 videoURL: fdsf
 ---
--- pages/recipe-mock-up.js
+++ pages/recipe-mock-up.js
@@ -1,53 +1,58 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import {
+  attributes,
+  react as recipeContent,
+} from '../content/recipes/easy-slow-cooker-artichoke-garlic-chicken.md';
 
 const RecipeMockUp = () => {
+  let {
+    title,
+    author,
+    summary,
+    dairyFree,
+    servings,
+    prepTime,
+    cookTime,
+    featuredImage,
+    ingredients,
+    instructions,
+    videoTitle,
+    videoURL,
+  } = attributes;
+  console.log('ingredients', ingredients);
+  const ingredientList = ingredients.map((ingredient, index) => {
+    return <li key={index}>{ingredient.ingredient}</li>;
+  });
   return (
     <Container className="mt-4">
       <Row>
         <Col md={8}>
-          <img
-            src="img/freeStockImages/street-food-feast-stockcake.jpg"
-            fluid
-          />
+          <img src={featuredImage} fluid />
         </Col>
         <Col md={4}>
-          <h1>Lorem Ipsum</h1>
-          <p>Published by: Tasty Publications</p>
-          <p>Prep Time: 30 mins</p>
-          <p>Servings: 2</p>
-          <p>"Cooking Book" by Author</p>
+          <h1>{title}</h1>
+          <p>Published by: {author}</p>
+          <p>Prep Time: {prepTime} mins</p>
+          <p>Cook Time: {cookTime} mins</p>
+          <p>Servings: {servings}</p>
+          {/* Replace dairy free string to be image/logo */}
+          <p>{dairyFree == true ? 'Dairy Free' : 'Not Dairy Free'}</p>
         </Col>
       </Row>
+
+      {/* Fix spacing for summary to be in a row format */}
+      <Col md={6}>
+        <h1>Summary</h1>
+        {/* <p>{summary}</p> */}
+      </Col>
       <Row className="mt-4">
         <Col md={6}>
           <h3>Ingredients</h3>
-          <ul>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-          </ul>
+          <ul>{ingredientList}</ul>
         </Col>
         <Col md={6}>
           <h3>Cooking Instructions</h3>
-          <ol>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-          </ol>
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        <Col>
-          <h3>Recipe Tutorial</h3>
-          <img src="img/freeStockImages/streetfood.jpg" />
+          <h2></h2>
         </Col>
       </Row>
     </Container>
