b5c500d9d2a2aafc7fbbb70e707c97d2973e22e3
Jerry Sirisavath <Jsirisavath123@gmail.com>
2024-09-13 15:59:45 -0500

    Fix search feature

--- pages/recipe/index.js
+++ pages/recipe/index.js
@@ -23,37 +23,98 @@ const importRecipeData = async () => {
   );
 };
 
-const filterRecipeData = (recipeLists, keyWordSearch) => {
-  const fuse = new Fuse(recipeLists, {
-    keys: ['title'],
+const filterRecipeData = (recipeLists, keyword) => {
+  console.log('Recipes Lists', recipeLists);
+
+  const filteredRecipes = recipeLists.filter((recipe) => {
+    const lowerCaseTitle = recipe.attributes.title.toLowerCase();
+    return lowerCaseTitle.includes(keyword.toLowerCase());
   });
 
-  const searchResult = fuse.search(keyWordSearch);
+  console.log('Filtered Recipes:', filteredRecipes);
 
-  return searchResult;
+  if (filteredRecipes.length === 0) {
+    console.warn('No matching recipes found');
+  }
+
+  return filteredRecipes;
 };
 
 export default class Recipe extends Component {
   static async getInitialProps() {
-    const recipeList = await importRecipeData();
+    try {
+      const recipeList = await importRecipeData();
+      console.log('Recipe List:', recipeList);
+      return { recipeList };
+    } catch (error) {
+      console.error('Error fetching recipe data:', error);
+      return {};
+    }
+  }
 
-    return { recipeList };
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterRecipes: props.recipeList,
+      searchTerm: '',
+    };
+  }
+
+  componentDidMount() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const value = urlParams.getAll('q');
+
+    if (value && value.length > 0) {
+      this.filterRecipes(value[0]);
+    } else {
+      this.setState({
+        filteredRecipes: this.props.recipeList,
+        searchTerm: '',
+      });
+    }
+  }
+
+  filterRecipes(keyword) {
+    const filteredRecipes = filterRecipeData(this.props.recipeList, keyword);
+    console.log('Filtered Recipes:', filteredRecipes);
+
+    this.setState({ filteredRecipes });
   }
 
   render() {
     const { recipeList } = this.props;
+    const { filteredRecipes, searchTerm } = this.state;
+
+    console.log('Current state:', {
+      filteredRecipes,
+      searchTerm,
+      recipeList,
+    });
+
+    if (!recipeList || recipeList.length === 0) {
+      return <div>No recipes found</div>;
+    }
+
     return (
       <>
-        <SearchBarWithIcon />
+        <SearchBarWithIcon
+          placeholder={searchTerm || 'Search...'}
+          onSearch={(keyword) => {
+            this.filterRecipes(keyword);
+          }}
+        />
         <div className="recipe-list">
-          {recipeList.map((recipe, key) => {
-            return (
+          {filteredRecipes && filteredRecipes.length > 0 ? (
+            filteredRecipes.map((recipe, key) => (
               <Link href={`recipe/${recipe.slug}`} key={key}>
                 <img src={recipe.attributes.featuredImage} />
                 <h2>{recipe.attributes.title}</h2>
               </Link>
-            );
-          })}
+            ))
+          ) : (
+            <p>No matching recipes found.</p>
+          )}
         </div>
       </>
     );
